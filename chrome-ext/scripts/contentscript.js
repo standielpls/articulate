e26605ca0ddf6309a107318d68bc6fe15658f856
Stanley Chin <stanley.chin@crowdriff.com>
2020-04-14 23:21:07 -0400

    Reformat

--- chrome-ext/scripts/contentscript.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    if(message.type === 'fireContentScript') {
-        alert('Message Received By Page')
-        var btn = document.createElement("H1") 
-        var t = document.createTextNode("Here is a H1 injected by the Chrome Extension")      
-        btn.appendChild(t)
-        document.body.prepend(btn)
-    }     
-})
